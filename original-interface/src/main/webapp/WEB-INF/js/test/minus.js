ff86ff6a38643baf7a3068abfc720ec1815b66f1
whc <junnianchou@foxmail.com>
2019-05-15 00:33:07 +0800

    //stylelint css检测和修复的实现

--- original-interface/src/main/webapp/WEB-INF/js/test/minus.js
+++ original-interface/src/main/webapp/WEB-INF/js/test/minus.js
@@ -2,7 +2,7 @@ define(function(){
     var minus = {
         //两数相减
         minus: function(a, b){
-            console.log(a - b);
+            return a - b;
         }
     };
     return minus;
